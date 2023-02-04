fc04e41a2e67846b7278c78a7105d3f1bdd8b14c
nikhar <nikhar.gangwar13@gmail.com>
2023-02-05 00:33:09 +0530

    fix: route bug fixed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,9 +3,10 @@ const  companyController =require('../controllers/controllers.js');
 const companyRouter = express.Router();
 
 //routes
+companyRouter.get('/save',companyController.postCompanyDetailsController);
+
 companyRouter.get('/:id',companyController.getCompanyDetailsController);
 
-companyRouter.get('/save',companyController.postCompanyDetailsController);
 //userRouter.post('/',userController.postUserController);
 
 module.exports=companyRouter;
