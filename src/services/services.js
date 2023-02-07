09a488869918c37461f44d2f90a21fb746b9aa99
nikhar <nikhar.gangwar13@gmail.com>
2023-02-07 10:40:00 +0530

    feat: add service test cases

--- src/services/services.js
+++ src/services/services.js
@@ -1,8 +1,8 @@
 
 const axios = require('axios');
-const  db = require('../../models');
+const db = require('../../models');
 // const  {HTTPError} = require('../utils/httperror.js');
-const {HTTPError} = require('../../src/utils/httperror.js')
+const { HTTPError } = require('../../src/utils/httperror.js')
 const storeCompanyDetails = async () => {
 
     const companySectorCsvData = await axios.get('https://store-0001.s3.amazonaws.com/input.csv');
@@ -15,7 +15,7 @@ const storeCompanyDetails = async () => {
         const companySectorCsvDataArrayElementArray = companySectorCsvDataArrayElement.split(',');
         const companyId = companySectorCsvDataArrayElementArray[0];
         const companySector = companySectorCsvDataArrayElementArray[1];
-        
+
 
         companyDetailsById = await axios.get('http://54.167.46.10/company/' + companyId);
         const companyName = companyDetailsById.data.name;
@@ -23,29 +23,29 @@ const storeCompanyDetails = async () => {
 
 
         companyDetailsBySector = await axios.get('http://54.167.46.10/sector?name=' + companySector);
-         let companyScore = 0;
+        let companyScore = 0;
 
         companyDetailsBySector.data.map((company) => {
-             if (company.companyId === companyId) {
-                companyScore = ((company.performanceIndex[0].value * 10) + (company.performanceIndex[1].value / 10000) + (company.performanceIndex[2].value * 10) + (company.performanceIndex[3].value))/4;
-             }
-         });
-
-         const result = await db.CompanyDetails.create({
-            companyName: companyName,
-            ceoName: companyCeo,
-            companyScore:companyScore,
-            companySector:companySector
+            if (company.companyId === companyId) {
+                companyScore = ((company.performanceIndex[0].value * 10) + (company.performanceIndex[1].value / 10000) + (company.performanceIndex[2].value * 10) + (company.performanceIndex[3].value)) / 4;
+            }
         });
 
-        
+        const result = await db.CompanyDetails.create({
+            companyName: companyName,
+            ceoName: companyCeo,
+            companyScore: companyScore,
+            companySector: companySector
+        });
+
+
     }
 
-    const resultCompanyData = await db.CompanyDetails.findAll({attributes: ['id','companyName','companyScore']});
-    
-    if(resultCompanyData.length===0){
+    const resultCompanyData = await db.CompanyDetails.findAll({ attributes: ['id', 'companyName', 'companyScore'] });
+
+    if (resultCompanyData.length === 0) {
         throw new HTTPError(404, 'database is empty');
-     }
+    }
 
     return resultCompanyData;
 
@@ -53,31 +53,30 @@ const storeCompanyDetails = async () => {
 
 
 const getCompanyDetailsInRankingOrder = async (sector) => {
-    const resultCompanyData = await db.CompanyDetails.findAll({where:{companySector:sector},order: [['companyScore', 'DESC']]},{attributes: ['id','companyName','companyScore']});
-    if(resultCompanyData.length===0){
-       throw new HTTPError(404, 'No such company with given sector found');
+    const resultCompanyData = await db.CompanyDetails.findAll({ where: { companySector: sector }, order: [['companyScore', 'DESC']] }, { attributes: ['id', 'companyName', 'companyScore'] });
+    if (resultCompanyData.length === 0) {
+        throw new HTTPError(404, 'No such company with given sector found');
     }
 
-    resultCompanyData.forEach((element,index) => {
-        element.dataValues.rank = index+1;
+    resultCompanyData.forEach((element, index) => {
+        element.dataValues.rank = index + 1;
     });
 
     return resultCompanyData;
 }
 
 
-const updateCompanyDetailsInDb = async (companyId,ceoName) => {
+const updateCompanyDetailsInDb = async (companyId, ceoName) => {
     const resultCompanyData = await db.CompanyDetails.update({ ceoName: ceoName }, {
         where: {
             id: companyId
         }
     });
 
-    if(resultCompanyData[0]===0){
+    if (resultCompanyData[0] === 0) {
         throw new HTTPError(404, 'No such company found');
-     }
+    }
 
-   
     return resultCompanyData;
 }
-module.exports = { storeCompanyDetails ,getCompanyDetailsInRankingOrder,updateCompanyDetailsInDb};
+module.exports = { storeCompanyDetails, getCompanyDetailsInRankingOrder, updateCompanyDetailsInDb };
--- test/services.test.js
+++ test/services.test.js
@@ -1,6 +1,7 @@
 const services = require('../src/services/services.js');
 const { HTTPError } = require('../src/utils/httperror.js');
 const db = require('../models');
+
 describe('service testing to get company data', () => {
     it('should return all tasks', async () => {
         const testCode =[{"dataValues":{
@@ -23,3 +24,51 @@ describe('service testing to get company data', () => {
         );
     });
 })
+
+
+describe('service testing to get company data', () => {
+    it('should update given tasks', async () => {
+      
+        jest.spyOn(db.CompanyDetails, 'update').mockResolvedValue([1]
+        );
+     
+        const mockreq = {
+             params: {
+                id: 1,
+            },body: { ceoName: 'gopal' }
+        };
+
+        const result = await services.updateCompanyDetailsInDb(mockreq);
+        expect(result).toEqual(
+            [1]
+        );
+    });
+})
+
+
+// describe('service testing to get company data', () => {
+//     it('should throw error message if no such id exist', async () => {
+// const err = new HTTPError(404, 'No such company found');
+
+//         jest.spyOn(db.CompanyDetails, 'update').mockRejectedValue(
+//                [0]
+//             );
+//         const mockreq = {
+//              params: {
+//                 id: 143
+//             },body: { ceoName: 'gopal' }
+//         };
+
+//         const mockres = {
+//             status:jest.fn().mockReturnThis(),
+//             json: jest.fn()
+//         };
+
+//         await services.updateCompanyDetailsInDb(mockreq);
+
+//         expect(mockres.status).toHaveBeenCalledWith(404);       
+//         expect(mockres.json).toHaveBeenCalledWith({message:"No such company found"});
+      
+        
+//     });
+// })
